2c17ef9f5342b9036938c5ce36fa8d709a45e837
Miftahul Lubab <miftahul.lubab20@gmail.com>
2021-07-23 10:19:05 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -72,7 +72,7 @@ class NotesHandler {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
 
       const note = await this._service.getNoteById(id);
 
@@ -110,7 +110,7 @@ class NotesHandler {
 
       this._validator.validateNotePayload(request.payload);
 
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
 
       await this._service.editNoteById(id, request.payload);
 
